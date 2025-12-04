b6186958a5ce083d2a612badc9e80d7105f109b1
surya1729-prog <narusuryacharan@gmail.com>
2025-12-04 17:16:36 +0530

    FIX: Corrected case sensitivity in controller path

--- backend/src/routes/students.js
+++ backend/src/routes/students.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ctrl = require('../controllers/studentCtrl');
+const ctrl = require('../controllers/studentctrl');
 
 // GET all students
 router.get('/', ctrl.getStudents);
