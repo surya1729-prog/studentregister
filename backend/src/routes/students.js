51fdd010bd0310c79b169fb353e3a8f9aaafb8ae
surya1729-prog <narusuryacharan@gmail.com>
2025-12-04 17:51:44 +0530

    files renamed

--- backend/src/routes/students.js
+++ backend/src/routes/students.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ctrl = require('../Controllers/studentCtrl');
+const ctrl = require('../Controllers/studentCtrl.js');
 
 // GET all students
 router.get('/', ctrl.getStudents);
