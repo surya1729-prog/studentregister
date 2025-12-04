da7b24db498f9de73c3907a787c5d1e70b8a44dc
surya1729-prog <narusuryacharan@gmail.com>
2025-12-04 17:25:52 +0530

    changed the file names

--- backend/src/routes/students.js
+++ backend/src/routes/students.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ctrl = require('../controllers/studentctrl');
+const ctrl = require('../Controllers/studentCtrl');
 
 // GET all students
 router.get('/', ctrl.getStudents);
